f8573c7d1232dcfc0e55a6449097aa0514d50d75
Jeo D <e8gea6w5z8id@opayq.com>
2019-03-12 22:57:54 -0600

    Add endpoint for filtering cards

--- db/queries.js
+++ db/queries.js
@@ -7,7 +7,7 @@ const getAllSets = async (req, res) => {
     const sets = await db('sets').select();
     res.status(200).json(sets);
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
 }
 
@@ -16,11 +16,21 @@ const getAllCards = async (req, res) => {
     const sets = await db('cards').select();
     res.status(200).json(sets);
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
+  }
+}
+
+const getCardsByQuery = async (req, res) => {
+  try {
+    const cards = await db('cards').select().where(req.query);
+    res.status(200).json(cardsg);
+  } catch (error) {
+    res.status(500).json(error);
   }
 }
 
 module.exports = {
   getAllSets,
-  getAllCards
+  getAllCards,
+  getCardsByQuery
 }
--- routes/api/v1.js
+++ routes/api/v1.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../../db/queries');
 
 router.get('/sets', db.getAllSets);
+router.get('/cards?', db.getCardsByQuery);
 router.get('/cards', db.getAllCards);
 
 module.exports = router;
